eaff1fc0a47a8e46d6ee255ceb6faf94ddb98b9b
SSKnT <sohaibshaukat78@gmail.com>
2025-06-17 13:46:37 +0500

    Update profile image source to me1.jpeg

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -166,7 +166,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.jpeg"
+              src="../../images/me1.jpeg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
